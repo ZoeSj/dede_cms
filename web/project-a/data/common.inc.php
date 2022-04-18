f704e0bd155edbe1324d947d2da95dea4b34a988
‘ZoeSj’ <shengjiamo@163.com>
2022-04-18 18:18:50 +0800

    git init

---
+++ web/project-a/data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = 'php-mysql57';
+$cfg_dbname = 'dede_cms';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = '123456';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
